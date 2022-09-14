6f0a22baa726bc6342d257ec30263c10c7344f90
Raghu K <raghuk.vit@gmail.com>
2022-09-14 08:38:48 +0530

    Update terraform-mutable-infra/

--- main.tf
+++ main.tf
@@ -10,14 +10,14 @@ module "vpc" {
 }
 
 module "docdb" {
-  source           = "./vendor/modules/docdb/"
-  env              = var.env
-  engine_version   = var.docdb_engine_version
-  apps_subnets_ids = local.apps_subnets_ids
-  vpc_id           = module.vpc.vpc_id
-  vpc_cidr_block   = var.vpc_cidr_block
-  instance_count   = var.docdb_cluster_instances_count
-  instance_class   = var.docdb_instance_class
+  source         = "./vendor/modules/docdb/"
+  env            = var.env
+  engine_version = var.docdb_engine_version
+  db_subnets_ids = local.db_subnets_ids
+  vpc_id         = module.vpc.vpc_id
+  vpc_cidr_block = var.vpc_cidr_block
+  instance_count = var.docdb_cluster_instances_count
+  instance_class = var.docdb_instance_class
 }
 
 
