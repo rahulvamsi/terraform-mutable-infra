478fae8710bdc3e21de561a5b0fd19a4baef458d
Raghu K <raghuk.vit@gmail.com>
2022-09-16 08:33:46 +0530

    Update infra/terraform-mutable-infra/

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,15 @@
 output "app_subnets" {
   value = local.apps_subnets_ids
 }
+
+output "vpc_id" {
+  value = module.vpc.vpc_id
+}
+
+output "workspace_id" {
+  value = var.WORKSTATION_IP
+}
+
+output "vpc_cidr" {
+  value = var.vpc_cidr_block
+}
